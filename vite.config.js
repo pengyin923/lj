be8b6ebce3a3e5440f41086ab47235ed817e3d7c
pengyin923@sina.com <pengyin923@sina.com>
2025-04-16 12:23:01 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'lj',
   plugins: [
     vue(),
     // vueDevTools(),
